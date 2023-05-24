4170d62a2d5663f33dc98af03392d70f1d6d0d9c
Pedro Henrique de Oliveira <90910198+opedroho@users.noreply.github.com>
2023-05-24 14:27:08 +0000

    (24/05) Continuação Projeto Karangos

--- karangos/src/components/ui/TopBar.jsx
+++ karangos/src/components/ui/TopBar.jsx
@@ -3,7 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import logo from '../../../public/karangos-logo-600px.png'
-import MainMenu from './MainMenu'
+import MainMenu from './MainMenu/MainMenu'
 
 export default function TopBar() {
   return (
