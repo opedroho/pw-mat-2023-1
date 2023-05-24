9b77ed82c536f0c4cfd1d358a5a736e9b7c3f083
Pedro Henrique de Oliveira <90910198+opedroho@users.noreply.github.com>
2023-05-24 14:26:05 +0000

    (24/05) Continuação Projeto Karangos

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -3,14 +3,15 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import TopBar from './Components/UI/Topbar'
+import TopBar from './components/ui/TopBar'
 import theme from './utils/theme'
 import { ThemeProvider } from '@mui/material/styles'
 import Box from '@mui/material/Box'
-import FooterBar from './Components/UI/footerbar'
-import { CssBaseline } from '@mui/material'
+import FooterBar from './components/ui/FooterBar'
+import CssBaseline from '@mui/material/CssBaseline'
 
 import Homepage from './pages/Homepage'
+import CustomersList from './pages/CustomersList'
 
 function App() {
   return (
@@ -18,25 +19,29 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Box sx={{
-            width: '100vw',
-            height: '100vh',
-            backgroundColor: 'background.default'
+          <Box sx={{ 
+            width: '100vw', 
+            minHeight: '100vh', 
+            backgroundColor: 'background.default' 
           }}>
             <TopBar />
             <Box sx={{
-              margin: '25px, 25px 55px 25px',
-              padding: '25px'
+              margin: '25px 25px 55px 25px'
             }}>
 
               <Routes>
-                <Route path='/' element={<Homepage />} />
+                <Route path="/" element={ <Homepage /> } />
               </Routes>
-              </Box>
-              <FooterBar />
+
+              <Routes>
+                <Route path="/customers" element={ <CustomersList /> } />
+              </Routes>
+
             </Box>
+            <FooterBar />
+          </Box>
         </ThemeProvider>
-      </BrowserRouter >
+      </BrowserRouter>
     </>
   )
 }
